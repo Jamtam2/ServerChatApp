37404654a6fc70bd105a2b7d4fcb265da736c158
Jamtam2 <kazmishahzeb7u@gmail.com>
2022-11-20 14:59:24 -0600

    added private messaging

--- Code/server.py
+++ Code/server.py
@@ -16,7 +16,7 @@ print("Initializing server..... \n IP: localhost \n Port#: 1234")
 #Bind serve_socket and listen for connections
 serv_socket.bind((serv_host, serv_port)) 
 serv_socket.listen(10)
-client_list=[]
+fullclient_list=[]
 client_names =[]
 
 def receive_send(client):
@@ -36,24 +36,42 @@ def receive_send(client):
     #Receives message from client and sends to all other clients
     while True:
             try:     
-                message = client.recv(2048)
-                broadcast(message)
+                message = client.recv(2048).decode()
+                wordmessage = message.split()
+
+                if wordmessage[2] == "-message":
+
+                    client_name = wordmessage[3]
+                    print("messaging: ", client_name)
+                    
+                    if client_name in client_names:
+                        clientindex = client_names.index(client_name)
+                        print(clientindex)
+                        privatemsglist = [fullclient_list[clientindex]]
+                        print(privatemsglist)
+                        message =f"Private Message from{wordmessage[0]}:  \n " + "".join(wordmessage[4:])
+                        broadcast(message.encode(),privatemsglist)
+                    else:
+                        print("Error: Client not in list, message has not been sent")
+
+                else:
+                    broadcast(message.encode(),fullclient_list)
                 
                 
             except:
                 #remove client from lists and close connection if client exited/error
-                listindex = client_list.index(client)
+                listindex = fullclient_list.index(client)
                 nameofclient = client_names[listindex]
-                broadcast(f"{nameofclient} has left the server".encode())
+                broadcast(f"{nameofclient} has left the server".encode(), fullclient_list)
                 client.close()
 
                 client_names.remove(nameofclient)
-                client_list.remove(client)
+                fullclient_list.remove(client)
                 
                 break
                 
 #Send message to all clients
-def broadcast(message):
+def broadcast(message,client_list):
     for client in client_list:
             try:
                 client.send(message)
@@ -69,7 +87,7 @@ def intialize_server():
 
         #add client address to list
         print(address, " connected")
-        client_list.append(client)
+        fullclient_list.append(client)
 
         #get a name to call the client that isnt address
         client.send("name".encode())
@@ -79,7 +97,7 @@ def intialize_server():
             pass
         else:
             print(client_names)
-        broadcast(f"{name} has connected to the server".encode())
+        broadcast(f"{name} has connected to the server".encode(),fullclient_list)
         print("You are now able to chat!")
 
         thread = threading.Thread(target=receive_send, args=((client,)))
